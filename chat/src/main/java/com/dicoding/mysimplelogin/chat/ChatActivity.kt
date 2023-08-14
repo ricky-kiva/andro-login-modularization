0e8874d451323c21686df989254082a99caeeb31
ricky-kiva <ricky.kiva@gmail.com>
2023-08-14 15:06:26 +0700

    feat(presentation): add algorithm for ChatActivity

--- chat/src/main/java/com/dicoding/mysimplelogin/chat/ChatActivity.kt
+++ chat/src/main/java/com/dicoding/mysimplelogin/chat/ChatActivity.kt
@@ -2,10 +2,19 @@ package com.dicoding.mysimplelogin.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.rickyslash.core.SessionManager
+import com.rickyslash.core.UserRepository
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val tvChat = findViewById<TextView>(R.id.tv_chat)
+        val sesi = SessionManager(this)
+        val userRepository = UserRepository.getInstance(sesi)
+        tvChat.text = "Hello ${userRepository.getUser()}! \n Welcome to Chat Feature"
+
     }
 }
