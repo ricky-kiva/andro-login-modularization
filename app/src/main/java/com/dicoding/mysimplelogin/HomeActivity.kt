ed1873b34fd0e4ce25895218c548700abc23225c
ricky-kiva <ricky.kiva@gmail.com>
2023-08-14 15:18:39 +0700

    feat(presentation-home): add FAB on HomeActivity

--- app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.dicoding.mysimplelogin
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
 import com.rickyslash.core.SessionManager
@@ -25,10 +26,23 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        binding.fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun moveToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
+    }
+
 }
